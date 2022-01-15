7e4434cdd977c61c197c6fd2099e9cb49c3a14e2
Kodesh Pupik <kpupik@jeeng.com>
2022-01-15 21:02:28 +0200

    add ingress

--- main_auto_scale.tf
+++ main_auto_scale.tf
@@ -4,13 +4,58 @@ resource "aws_key_pair" "key" {
 }
 
 
-resource "aws_instance" "web_app-2" {
+resource "aws_vpc" "my_vpc" {
+  cidr_block = var.vpc
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.my_vpc.id
+}
+
+
+resource "aws_security_group" "allow_web" {
+  name        = "allow_web"
+  description = "Allow web traffic"
+  vpc_id      = aws_vpc.my_vpc.id
+
+  ingress {
+    description = "HTTPS traffic"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "HTTP traffic"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
+
+
+resource "aws_instance" "web_app_2" {
   ami               = var.instance_name
   instance_type     = "t3.micro"
   availability_zone = var.availability_zone
+  key_name          = var.key_name
   tags = {
     Name = "auto_scale_instance"
   }
   user_data = file("install_web_server.sh")
 }
 
+
+output "public_ip" {
+  value = aws_instance.web_app_2.public_ip
+}
