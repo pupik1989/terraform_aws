511258770f8e1df5068b52cd7d907a464b12bec0
Kodesh Pupik <kpupik@jeeng.com>
2022-01-15 17:56:41 +0200

    add key

--- main_auto_scale.tf
+++ main_auto_scale.tf
@@ -1,15 +1,16 @@
 resource "aws_key_pair" "key" {
   key_name   = var.key_name
-  public_key = var.public_key
+  public_key = file(var.public_key)
 }
 
 
 resource "aws_instance" "web_app-2" {
-  ami           = var.instance_name
-  instance_type = "t3.micro"
+  ami               = var.instance_name
+  instance_type     = "t3.micro"
   availability_zone = var.availability_zone
   tags = {
     Name = "auto_scale_instance"
   }
+  user_data = file("install_web_server.sh")
 }
 
